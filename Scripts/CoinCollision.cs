2ce918093ff54d3dbc8634d1d682f64217398b26
04-Nazmy <114890353+04-Nazmy@users.noreply.github.com>
2022-10-06 11:35:16 +0800

    Completed coin and restart scripts
    now doing timer script

--- Scripts/CoinCollision.cs
+++ Scripts/CoinCollision.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CoinCollision : MonoBehaviour
 {
+    public GameObject coinsCollected;
+    private int coinCount=10;
+    private int totalCoin;
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (totalCoin == coinCount)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            coinCount++;
+            coinsCollected.GetComponent<Text>().text = "Score:" + coinCount;
+            Destroy(other.gameObject);
+        }
     }
 }
