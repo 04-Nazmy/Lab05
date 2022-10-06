d64fdd640d4c24874f71bc41245c7fa0ee5857f3
04-Nazmy <114890353+04-Nazmy@users.noreply.github.com>
2022-10-06 12:17:59 +0800

    inputted timer and particle effects

--- Scripts/CoinCollision.cs
+++ Scripts/CoinCollision.cs
@@ -8,11 +8,19 @@ public class CoinCollision : MonoBehaviour
 {
     public GameObject coinsCollected;
     private int coinCount=10;
-    private int totalCoin;
+    private float totalCoin;
+    private float scoreValue = 60;
+    private float timeleft;
+    private int timeRemaining;
+    public Text TimerText;
+    private float TimerValue = 20;
+    public ParticleSystem part;
     // Start is called before the first frame update
     void Start()
     {
         totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+        ParticleSystem part = GetComponent<ParticleSystem>();
+        timeleft = 20f;
     }
 
     // Update is called once per frame
@@ -27,6 +35,23 @@ public class CoinCollision : MonoBehaviour
         {
             SceneManager.LoadScene("GameLose");
         }
+
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        TimerText.text = "00:00:00" + timeRemaining.ToString();
+
+        if(scoreValue == totalCoin)
+        {
+           if(timeleft <= TimerValue)
+            {
+                SceneManager.LoadScene("GameWin");
+            }
+        }
+        else if(timeleft <= 0)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+ 
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,6 +60,7 @@ public class CoinCollision : MonoBehaviour
         {
             coinCount++;
             coinsCollected.GetComponent<Text>().text = "Score:" + coinCount;
+            part.Play();
             Destroy(other.gameObject);
         }
     }
